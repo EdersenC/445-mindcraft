ca9f5db40e9db28b6dd9bf159b6190e733fdea1b
Edersen Cadeau <edersenc@gmail.com>
2025-05-10 23:06:57 -0400

    Auto profiles

--- settings.js
+++ settings.js
@@ -1,8 +1,26 @@
+import fs from 'fs';
+import path from 'path';
+
+function getProfiles(dir) {
+
+    const profilesDir = path.join(dir, 'infinity');
+    const profiles = [];
+    fs.readdirSync(profilesDir).forEach(file => {
+        if (file.endsWith('.json')) {
+            profiles.push('.\\'+path.join(profilesDir, file));
+        }
+    });
+
+    console.log(profiles);
+    return profiles;
+}
+
+getProfiles('./profiles/');
 const settings = {
     "minecraft_version": "1.21.1", // supports up to 1.21.1
-    "host": "127.0.0.1", // or "localhost", "your.ip.
+    "host": "192.168.1.215", // or "localhost", "your.ip.
     // address.here"
-    "port": 55916,
+    "port": 25565,
     "auth": "offline", // or "microsoft"
 
     // the mindserver manages all agents and hosts the UI
@@ -12,21 +30,10 @@ const settings = {
     
     // the base profile is shared by all bots for default prompts/examples/modes
     "base_profile": "./profiles/defaults/survival.json", // also see creative.json, god_mode.json
-    "profiles": [
-        // "./andy.json",
-        // "./profiles/gpt.json",
-        // "./profiles/claude.json",
-        // "./profiles/gemini.json",
-        // "./profiles/freeguy.json",
-        "./profiles/llama.json", // llama3.1
-        // "./profiles/qwen.json",
-        "./profiles/grok.json",
-        // "./profiles/mistral.json",
-        // "./profiles/deepseek.json",
-
-        // using more than 1 profile requires you to /msg each bot indivually
-        // individual profiles override values from the base profile
-    ],
+    "profiles": getProfiles('./profiles/'), // list of bot profiles to load. if empty, it will use the base profile
+   // "profiles": [
+   //     "./profiles/llama.json",
+   // ],
     "load_memory": false, // load memory from previous session
     "init_message": "Respond with hello world and your name", // sends to all on spawn
     "only_chat_with": [], // users that the bots listen to and send general messages to. if empty it will chat publicly
@@ -40,8 +47,8 @@ const settings = {
     "code_timeout_mins": -1, // minutes code is allowed to run. -1 for no timeout
     "relevant_docs_count": 5, // number of relevant code function docs to select for prompting. -1 for all
 
-    "max_messages": 30, // max number of messages to keep in context
-    "num_examples": 15, // number of examples to give to the model
+    "max_messages": 20, // max number of messages to keep in context
+    "num_examples": 10, // number of examples to give to the model
     "max_commands": -1, // max number of commands that can be used in consecutive responses. -1 for no limit
     "verbose_commands": true, // show full command syntax
     "narrate_behavior": true, // chat simple automatic actions ('Picking up item!')
